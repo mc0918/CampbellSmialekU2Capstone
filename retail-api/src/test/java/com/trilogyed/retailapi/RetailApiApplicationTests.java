5e0c2b57ef6e50d537e662636ac598853da13f14
Michael Campbell <mcampbell0918@gmail.com>
2019-12-06 11:56:59 -0600

    created retail-api project

---
+++ retail-api/src/test/java/com/trilogyed/retailapi/RetailApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trilogyed.retailapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

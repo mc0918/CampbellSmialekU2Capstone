61d72e3475d86d6559e2f4617660ff48abdc4544
Michael Campbell <mcampbell0918@gmail.com>
2019-12-06 09:40:56 -0600

    created level up service

---
+++ level-up-service/src/test/java/com/trilogyed/levelupservice/LevelUpServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trilogyed.levelupservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevelUpServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
